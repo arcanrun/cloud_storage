1c72e59eb5db796b6fd19b6ccd3b46bd5991c014
arcanrun <arcan.run@gmail.com>
2020-06-19 14:25:55 +0300

    lesson01.1

--- client/src/main/java/fxclient/Questions.java
+++ client/src/main/java/fxclient/Questions.java
@@ -3,19 +3,12 @@ package fxclient;
 public class Questions {
     //!
     //Как и в каком виде передавать файлы?
-    // -- В виде простого потока байтов, использовать потоки ввода/вывода
+    // - В виде простого потока байтов, использовать потоки ввода/вывода
+    // - сериализацией
 
     //Как пересылать большие файлы?
-    //использовать буфер, например использовать в качестве буффера байт массив
-    //                    byte [] buffer = new byte[1024];
-    //                    File file = new File(dir + "\\" + fileName);
-    //                    FileOutputStream fos = new FileOutputStream(file);
-    //                    for (long i = 0; i <= fileLength / 1024; i++) {
-    //                        int read = in.read(buffer);
-    //                        System.out.println("bytes readed!");
-    //                        fos.write(buffer, 0, read);
-    //                        fos.flush();
-    //
+    // - разбивать на куски и отправлять кусками
+
 
     //Как пересылать служебные команды?
     // --  Например, строка со специальным префиксом: /Send
